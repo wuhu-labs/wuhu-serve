e5f7ce199f137195e34a2299a4150a13d277b6c1
Liu, Minsheng <lambda@liu.ms>
2026-03-24 22:50:19 +0800

    Depend on wuhu-fetch 0.2.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/wuhu-labs/wuhu-fetch", branch: "codex/body-resolution"),
+    .package(url: "https://github.com/wuhu-labs/wuhu-fetch", .upToNextMinor(from: "0.2.0")),
     .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.0"),
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
   ],
